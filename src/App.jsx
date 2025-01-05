c72fb44beb71f21173eed4ff8457566050fab00e
xasanov274 <xasanovabdulaziz51@gmail.com>
2025-01-05 15:28:23 +0500

    img qoshdim

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
         <h1>hello uzb</h1>
         <h2>yunusabad welcome</h2>
         <a href="#">mars space</a>
+        <img src="https://thumbs.dreamstime.com/b/glitch-art-visual-form-uses-digital-errors-pixelated-graphics-artifacts-to-create-abstract-distorted-images-327802040.jpg" alt="hayot" />
      </div>
     </>
   )
