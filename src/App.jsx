13f1691459b45f4a0c877a1cc796ec91092d4b1d
xasanov274 <xasanovabdulaziz51@gmail.com>
2025-01-05 15:11:04 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+        <h1>hello uzb</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
